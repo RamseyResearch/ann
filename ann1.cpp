0dd014a1d1163577397b66efb277443de72d05b9
Alex Roberts <aroberts2@washcoll.edu>
2016-06-09 10:19:25 -0400

    an ann beginning - broken - first commit

---
+++ ann1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class ANN
+{
+private:
+	vector<vector<float>> biases;
+	vector<vecor<float>> 
+
+public:
+	ANN()
+	{
+
+	}
+
+
+};
+
+int main()
+{
+	cout << "yo";
+
+
+
+	system("PAUSE");
+}
