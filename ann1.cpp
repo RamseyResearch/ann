a98bca8dd2db8bb83f6e6a642ab6c27c3b236e24
Alex Roberts <aroberts2@washcoll.edu>
2016-06-10 16:25:46 -0400

    WORKING FOR PLANET ANN!

--- ann1.cpp
+++ ann1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using namespace std;
 
 class ANN
@@ -12,6 +13,7 @@ private:
 
 	vector<vector<float>> biases;
 	vector<vector<vector<float>>> weights;
+	vector<vector<float>> activations;
 
 public:
 
@@ -29,8 +31,16 @@ public:
 		for (int i = 0; i < numLayers; i++) {
 			vector<float> row;
 			for (int j = 0; j < lengths[i]; j++)
-				row.push_back(1);
+				row.push_back(i*j);  //was 1
 			biases.push_back(row);
+		}
+
+		//set all activations to 0
+		for (int i = 0; i < numLayers; i++) {
+			vector<float> row;
+			for (int j = 0; j < lengths[i]; j++)
+				row.push_back(0);
+			activations.push_back(row);
 		}
 
 		//set weights; weights[n][][] correspond to neurons in biases[n+1][]
@@ -74,6 +84,27 @@ public:
 		maxLength = 0;
 		for (int m = 0; m < numLayers; m++)
 			maxLength = (layerLengths[m] > maxLength ? layerLengths[m] : maxLength);
+
+		for (int i = 0; i < numLayers; i++) {
+			vector<float> row;
+			for (int j = 0; j < layerLengths[i]; j++)
+				row.push_back(0);
+			activations.push_back(row);
+		}
+	}
+
+	void printActivations()
+	{
+		for (unsigned j = 0; j < maxLength; j++) {
+			for (unsigned i = 0; i < activations.size(); i++) {
+				if (j >= activations[i].size())
+					cout << "  ";
+				else
+					cout << activations[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
 	}
 
 	//print all biases; first layer is Xs
@@ -120,14 +151,48 @@ public:
 		}
 		cout << endl;
 	}
+
+	//Sigmoid/sigma function
+	float sigmoid(double z)
+	{
+		return 1 / (1 + exp(-z));
+	}
+
+	//Sigmoid derivative
+	float sigmoidPrime(double z)
+	{
+		return sigmoid(z)*(1 - sigmoid(z));
+	}
+
+	void feedForward(vector<float> &input)
+	{
+		activations[0] = input;
+
+		for (int i = 1; i < numLayers; i++) {
+			for (int j = 0; j < activations[i].size(); j++) {
+				float sum = 0;
+
+				for (int k = 0; k < activations[i - 1].size(); k++)
+					sum += weights[i - 1][k][j] * activations[i - 1][k];
+
+				activations[i][j] = sigmoid(sum + biases[i][j]);
+			}
+		}
+	}
 };
 
 int main()
 {
-	vector<int> len = {4, 3, 5, 1};
+	/*vector<int> len = {3, 2, 2};
 	ANN network = ANN(len);
 	network.printBiases();
 	network.printWeights();
+	network.printActivations();
+	vector<float> in = {1, 0, .001f};
+	network.feedForward(in);
+	network.printActivations();
+
+	cout << "----------------------------------------------------" << endl;
 
 	vector<vector<float>> bB = { {0, 0, 0}, {1, 2} };
 	vector<vector<vector<float>>> wW = { {{1, 2},{3, 4},{5, 6}} };
@@ -135,6 +200,23 @@ int main()
 	ANN network2 = ANN(wW, bB);
 	network2.printBiases();
 	network2.printWeights();
+	network2.printActivations();
+	vector<float> in2 = {0, 3, .002f};
+	network2.feedForward(in2);
+	network2.printActivations();*/
+
+
+
+	vector<vector<float>> bB3 = { { 0, 0, 0 },{ 0, 4 }, {0, 0} };
+	vector<vector<vector<float>>> wW3 = { { { 1, 3 },{ -2, 2 },{ 3, 3} }, { {1, -1}, {-1, -2} } };
+
+	ANN network3 = ANN(wW3, bB3);
+	network3.printBiases();
+	network3.printWeights();
+	network3.printActivations();
+	vector<float> in3 = { 1, 0, 0};
+	network3.feedForward(in3);
+	network3.printActivations();
 
 	system("PAUSE");
 }
